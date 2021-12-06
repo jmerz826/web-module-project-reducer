14783826502ac46a25628da83ad0fb53e3d4a9d7
John Merz <jmerz826@gmail.com>
2021-12-06 17:15:42 -0500

    each button adds to displayed total

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,11 @@ function App() {
   // useReducer hook
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleClick = () => {
-    dispatch(addOne());
+  // const handleClick = () => {
+  //   dispatch(addOne());
+  // }
+  const handleClick = (num) => {
+    dispatch(applyNumber(num));
   }
 
   return (
@@ -39,21 +42,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={ handleClick}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => {handleClick(1)}}/>
+              <CalcButton value={2} onClick={() => {handleClick(2)}}/>
+              <CalcButton value={3} onClick={() => {handleClick(3)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => {handleClick(4)}}/>
+              <CalcButton value={5} onClick={() => {handleClick(5)}}/>
+              <CalcButton value={6} onClick={() => {handleClick(6)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => {handleClick(7)}}/>
+              <CalcButton value={8} onClick={() => {handleClick(8)}}/>
+              <CalcButton value={9} onClick={() => {handleClick(9)}}/>
             </div>
 
             <div className="row">
